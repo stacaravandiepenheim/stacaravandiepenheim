81760803e57f6438b378bed3588049a00391ffdb
Annegreet <peterenannegreet@gmail.com>
2026-06-17 19:27:49 +0200

    augustus

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
   addDateRange('2026-06-15', '2026-06-25', 'bezet');       // verhuurd / bezet
   addDateRange('2026-07-03', '2026-07-11', 'bezet');       // verhuurd / bezet
   addDateRange('2026-08-10', '2026-08-13', 'bezet');       // verhuurd / bezet
-  addDateRange('2026-07-12', '2026-08-06', 'unavailable'); // zomervakantie noord (niet te boeken)
+  addDateRange('2026-07-12', '2026-08-02', 'unavailable'); // zomervakantie noord (niet te boeken)
   addDateRange('2026-10-23', '2027-04-02', 'unavailable'); // winter dicht
   // ----------------------
   // Prijzen
@@ -153,7 +153,7 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       name: 'Zomervakantie 2026 Bezet',
       start: '2026-07-12',
-      end: '2026-08-06',
+      end: '2026-08-02',
       night_mon_thu: null,
       night_fri: null,
       night_sat: null,
@@ -161,7 +161,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       name: 'Zomervakantie 2026',
-      start: '2026-08-06',
+      start: '2026-08-03',
       end: '2026-08-29',
       night_mon_thu: 55,
       night_fri: 55,
